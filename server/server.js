12b1d93915c27a94694ec7039e62a24f27e7befe
Kotaro WAJKI <beatenavenue@gmail.com>
2026-06-07 19:41:38 +0900

    fix server path

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '../src')));
+app.use(express.static(path.join(__dirname, '../docs')));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
